65bb4cd8da20a973e821a654bcfaaaa0e85de72e
Koay Yeow Teik <S10242544@connect.np.edu.sg>
2022-11-24 23:56:43 +0800

    test 2

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function Checkemail(value){
-    return false
+function checklogin(email,password){
+    console.log(email,password);
+    for (let i = 0;i<userlist.length;i++){
+        console.log(userlist[i])
+        if (userlist[i].email == email && userlist[i].password == password){
+            console.log("hi");
+            document.location.href = "index.html";
+        }
+    }
+    return false;// print invalid username here
 }
-function CheckPassword(value){
-    return false
+
+let userlist = [];
+function user(email,password){
+    this.email = email;
+    this.password = password;
+    userlist.push(this);
 }
+let name = new user("something","something")
